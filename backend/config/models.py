80cac97f411a79dff8b08754facd5aff505d40e5
gowthamthibhu <146013009+gowthamthibhu@users.noreply.github.com>
2025-04-06 10:38:49 +0530

    bug fixed

--- backend/config/models.py
+++ backend/config/models.py
@@ -4,9 +4,8 @@ class NavigationItem(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
     icon = models.CharField(max_length=100, blank=True, null=True)
     text = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)  # e.g., 'sidebarMenuItems', 'topNavTabs', etc.
-    parent_id = models.CharField(max_length=50, blank=True, null=True)  # For nested structures like connectionCards
-    image = models.ImageField(upload_to='images/', null=True, blank=True)  # Add this field
-        
+    category = models.CharField(max_length=50) 
+    parent_id = models.CharField(max_length=50, blank=True, null=True)  
+    image = models.ImageField(upload_to='images/', null=True, blank=True) 
     def __str__(self): 
         return self.text
--- frontend/osdag-frontend/src/pages/beam-to-column-connection.js
+++
@@ -1,5 +0,0 @@
-// pages/beam-to-column-connection.js
-import ConnectionDesigner from '../components/BeamToColumnConnection';
-
-export default function BeamToColumnConnectionPage() {
-}
